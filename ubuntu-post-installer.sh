429214f5a2c51d988c335f84d4b606b34d9ec861
acjminer <160790501+acjminer@users.noreply.github.com>
2024-03-06 16:04:35 -0600

    Update ubuntu-post-installer.sh

--- ubuntu-post-installer.sh
+++ ubuntu-post-installer.sh
@@ -18,7 +18,7 @@ wget https://repo.radeon.com/amdgpu-install/21.50/ubuntu/focal/amdgpu-install_21
 
 sudo dpkg -i amdgpu-install_21.50.50000-1_all.deb 
 sudo apt --fix-broken install
-sudo dpkg -i amdgpu-install_21.50.50000-1_all.deb -y
+sudo dpkg -i amdgpu-install_21.50.50000-1_all.deb -ysudo
 sudo amdgpu-install --usecase=graphics,opencl,openclsdk --no-dkms --no-32 --accept-eula -y
 sudo apt install "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)" -y
 sudo usermod -a -G render,video $LOGNAME 
@@ -30,10 +30,3 @@ wget https://github.com/CoinFuMasterShifu/janusminer/releases/download/0.2.5/jan
 
 #executable permission 
 chmod +x janusminer-ubuntu20
-
-#Create Minerscreen 
-
-screen -S miner
-
-#run janusminer
-./janusminer-ubuntu20 -a 059cfbc69c57c126f224119561c7921596584f373124718a -h 192.168.1.94
