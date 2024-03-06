715e54240fa3dda9986b89435f3169b5e049d00d
acjminer <160790501+acjminer@users.noreply.github.com>
2024-03-06 15:08:46 -0600

    Update ubuntu-post-installer.sh

--- ubuntu-post-installer.sh
+++ ubuntu-post-installer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #fix Broken install
-sudo apt --fix-broken install -y
+sudo apt --fix-broken install
 #update 
 sudo apt update; sudo apt upgrade -y
 
 #fix any broken packages 
-apt-get -f install -y
+sudo apt-get -f install -y
 
 #install clinfo
 sudo apt install clinfo -y
@@ -17,9 +17,11 @@ wget https://repo.radeon.com/amdgpu-install/21.50/ubuntu/focal/amdgpu-install_21
 #install amd drivers
 
 sudo dpkg -i amdgpu-install_21.50.50000-1_all.deb 
+sudo apt --fix-broken install
+sudo dpkg -i amdgpu-install_21.50.50000-1_all.deb 
 sudo amdgpu-install --usecase=graphics,opencl,openclsdk --no-dkms --no-32 --accept-eula -y
 sudo apt install "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)" -y
-sudo usermod -a -G render,video $LOGNAME -y
+sudo usermod -a -G render,video $LOGNAME 
 sudo apt install amdgpu-dkms -y
 
 #Download janusminer
